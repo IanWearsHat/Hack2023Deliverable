8bf5eb8b7a18642e4dc4947e316032cd57c1303a
Ian Dai <ianbbdai@gmail.com>
2023-11-10 01:40:28 -0800

    Styled Quotes

--- frontend/src/components/Quote/Quote.jsx
+++ frontend/src/components/Quote/Quote.jsx
@@ -3,7 +3,7 @@ import styles from './Quote.module.css';
 export default function Quote({name, quote, date}) {
     return (
       <div className={styles.quote}>
-        <p className={styles.quoteText}>"{quote}"</p>
+        <p className={styles.quoteText}>“{quote}”</p>
         <p className={styles.quoteDetails}>-{name} {date}</p>
       </div>
     );
